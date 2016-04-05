651b3f507beedc683e82fce64e86552ef7533078
Trisha <ptaylor403@gmail.com>
2016-04-05 16:13:37 -0400

    ai game loop

---
+++ game_of_sticks_ai_test.py
@@ -0,0 +1,17 @@
+import unittest
+from game_of_sticks_ai import *
+
+class TestGameOfSticks(unittest.TestCase):
+    def test_assert_equal_stick_count(self):
+        self.assertEqual(stick_count(10, 3), 7)
+
+    def test_assert_true_is_end(self):
+        self.assertTrue(is_end(2, 2))
+
+    def test_assert_false_is_end(self):
+        self.assertFalse(is_end(2, 3))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
